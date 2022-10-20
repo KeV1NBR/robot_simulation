1208744a5e56e1493c9e46e666b0c1d11c9cd4d2
kevin-6F-GPU <kevin-6F-GPU>
2022-10-20 18:22:37 +0800

    fix kinematics

--- pos.sh
+++ pos.sh
@@ -1,2 +1,2 @@
 
-rostopic pub /arm_controller/command trajectory_msgs/JointTrajectory '{joint_names: ["J1","J3","J5"], points: [{positions: [3.1416, 1.57097, 1.57097], time_from_start: [5,0]}]}' -1
+rostopic pub /arm_controller/command trajectory_msgs/JointTrajectory '{joint_names: ["J1","J3","J5"], points: [{positions: [3.1416, 1.57097, 1.57097], time_from_start: [10,0]}]}' -1
