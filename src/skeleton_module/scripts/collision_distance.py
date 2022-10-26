cc4942fa0aa715172baa2d01e6965fd281f88e99
kevin-6F-GPU <kevin-6F-GPU>
2022-10-26 19:01:46 +0800

    finish collision distance

--- src/skeleton_module/scripts/collision_distance.py
+++ src/skeleton_module/scripts/collision_distance.py
@@ -1,7 +1,7 @@
 import rospy
 import fcl
 import tf
-
+import numpy as np
 
 # human setting
 body = fcl.Cylinder(0.2, 0.6) #0
@@ -26,41 +26,116 @@ humanPartIndex = {0 : Collision("body", body), 1 : Collision("head", head), 2 : 
 robotPartIndex = {0 : Collision("Link2", Link2), 1: Collision("Link3", Link3), 2 : Collision("Link4", Link4), 3 : Collision("Link5", Link5)}
 
 class CollisionPair:
-    def __init__(self, humanNum, humanPart, robotPart):
-        self.humanNum = humanNum
-        self.humanPartInfo = humanPart
-        self.robotPartInfo = robotPart
-        self.info = fcl.DistanceResult()
-
+    def __init__(self, humanPartNum, robotPartNum):
+        self.humanPartNum = humanPartNum
+        self.robotPartNum = robotPartNum
     def update(self):
-        if(listener.canTransform('/world', 'human'+str(self.humanNum+1)+'_'+self.humanPartInfo.partName, rospy.Time(0))&listener.canTransform('/world', self.robotPartInfo.partName, rospy.Time(0))):
-            (humanTrans,humanRot) = listener.lookupTransform('/world', 'human'+str(self.humanNum+1)+'_'+self.humanPartInfo.partName, rospy.Time(0))
-            (robotTrans,robotRot) = listener.lookupTransform('/world', self.robotPartInfo.partName, rospy.Time(0))
-            humanTf = fcl.Transform(humanRot, humanTrans)
-            robotTf = fcl.Transform(robotRot, robotTrans)
-            request = fcl.DistanceRequest()
-            humanObject = fcl.CollisionObject(self.humanPartInfo.part, humanTf)
-            robotObject = fcl.CollisionObject(self.robotPartInfo.part, robotTf)
-            fcl.distance(humanObject, robotObject, request, self.info)
+        info = fcl.DistanceResult()
 
-    def getInfo(self):
-        return self.info
+        humanTf = humanTransforms[self.humanPartNum]
+        robotTf = robotTransforms[self.robotPartNum]
+        request = fcl.DistanceRequest()
+        humanObject = fcl.CollisionObject(humanPartIndex[self.humanPartNum].part, humanTf)
+        robotObject = fcl.CollisionObject(robotPartIndex[self.robotPartNum].part, robotTf)
+        fcl.distance(humanObject, robotObject, request, info)
+
+        return info
+
+
+robotTransforms = []
+humanTransforms = []
+
+def updateTransforms():
+    deg90 = np.array([0, 0.7071068, 0, 0.7071068])
+    global robotTransforms
+    global humanTransforms
+    listener = tf.TransformListener()
+    ret = True
+    listener.waitForTransform("/world", "/LINK2", rospy.Time(), rospy.Duration.from_sec(1))
+    if(listener.canTransform('/world', '/LINK2', rospy.Time(0))):
+        robotTransforms.clear()
+        (trans,rot) = listener.lookupTransform('/world', '/LINK2', rospy.Time(0))
+        robotTransforms.append(fcl.Transform(rot, trans))
+        (trans,rot) = listener.lookupTransform('/world', '/LINK3', rospy.Time(0))
+        robotTransforms.append(fcl.Transform(rot, trans))
+        (trans,rot) = listener.lookupTransform('/world', '/LINK4', rospy.Time(0))
+        robotTransforms.append(fcl.Transform(rot, trans))
+        (trans,rot) = listener.lookupTransform('/world', '/LINK5', rospy.Time(0))
+        robotTransforms.append(fcl.Transform(rot, trans))
+    else:
+        ret = False
+
+    if(listener.canTransform('/world', '/Spine_Naval0', rospy.Time(0))):
+        humanTransforms.clear()
+
+        (trans1,rot1) = listener.lookupTransform('/world', 'Spine_Naval0', rospy.Time(0))
+        (trans2,rot2) = listener.lookupTransform('/world', 'Spine_Chest0', rospy.Time(0))
+        trans1 = np.array(trans1)
+        trans2 = np.array(trans2)
+
+        humanTransforms.append(fcl.Transform(np.array(rot1)*deg90, (trans1+trans2)/2))
+
+        (trans1,rot1) = listener.lookupTransform('/world', 'Ear_Right0', rospy.Time(0))
+        (trans2,rot2) = listener.lookupTransform('/world', 'Ear_Left0', rospy.Time(0))
+        trans1 = np.array(trans1)
+        trans2 = np.array(trans2)
+        humanTransforms.append(fcl.Transform(rot1, (trans1+trans2)/2))
+
+        (trans1,rot1) = listener.lookupTransform('/world', 'Shoulder_left0', rospy.Time(0))
+        (trans2,rot2) = listener.lookupTransform('/world', 'Elbow_left0', rospy.Time(0))
+        trans1 = np.array(trans1)
+        trans2 = np.array(trans2)
+        humanTransforms.append(fcl.Transform(np.array(rot1)*deg90, (trans1+trans2)/2))
+
+        (trans1,rot1) = listener.lookupTransform('/world', 'Wrist_left0', rospy.Time(0))
+        trans1 = np.array(trans1)
+        humanTransforms.append(fcl.Transform(np.array(rot2)*deg90, (trans1+trans2)/2))
+
+        (trans2,rot2) = listener.lookupTransform('/world', 'Handtip_left0', rospy.Time(0))
+        trans2 = np.array(trans2)
+        humanTransforms.append(fcl.Transform(rot1, (trans1+trans2)/2))
+
+
+        (trans1,rot1) = listener.lookupTransform('/world', 'Shoulder_right0', rospy.Time(0))
+        (trans2,rot2) = listener.lookupTransform('/world', 'Elbow_right0', rospy.Time(0))
+        trans1 = np.array(trans1)
+        trans2 = np.array(trans2)
+        humanTransforms.append(fcl.Transform(np.array(rot1)*deg90, (trans1+trans2)/2))
+
+        (trans1,rot1) = listener.lookupTransform('/world', 'Wrist_right0', rospy.Time(0))
+        trans1 = np.array(trans1)
+        humanTransforms.append(fcl.Transform(np.array(rot2)*deg90, (trans1+trans2)/2))
+
+
+        (trans2,rot2) = listener.lookupTransform('/world', 'Handtip_right0', rospy.Time(0))
+        trans2 = np.array(trans2)
+        humanTransforms.append(fcl.Transform(rot1, (trans1+trans2)/2))
+
+    else:
+        ret = False
+
+    return ret
+
 
 if __name__ == '__main__':
     rospy.init_node('collision_distance')
-    listener = tf.TransformListener()
 
     pairList = []
     for  robotIt in range(4):
         for num in range(1):
             for humanIt in range(8):
-                pairList.append(CollisionPair(num, robotPartIndex[robotIt], humanPartIndex[humanIt]))
+                pairList.append(CollisionPair(humanIt, robotIt))
 
 
-    rate = rospy.Rate(10.0)
+    #rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
-        for it in pairList:
-            it.update()
-            print((it.humanPartInfo.partName) + ' ' + (it.robotPartInfo.partName) + ' : ' + str(it.getInfo().min_distance))
-        print('\n\n')
-        #(trans,rot) = listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
+        if updateTransforms():
+
+            rospy.loginfo('it')
+            for it in pairList:
+                info = it.update()
+                print((humanPartIndex[it.humanPartNum].partName) + ' ' + (robotPartIndex[it.robotPartNum].partName) + ' : ' ,info.min_distance)
+            print('\n\n')
+        else:
+            print('cant get tf')
+    #    rate.sleep()
